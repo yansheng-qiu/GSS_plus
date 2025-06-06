9791fa0129ba0e41088e028375c740ce9a966547
yansheng-qiu <qysgithubopen@gmail.com>
2025-06-06 12:21:03 +0800

    .sh

--- GSS_PLUS.sh
+++ GSS_PLUS.sh
@@ -15,22 +15,22 @@ export PATH=$pypath/include:$pypath/bin:$PATH
 export LD_LIBRARY_PATH=$pypath/lib:$LD_LIBRARY_PATH
 
 
-savepath=output_2020_GSS_0
+savepath=output_2020_GSS_plus_0
 pretrain="./pretrain/model_last_brast2020.pth" #https://drive.google.com/file/d/1jK9KAaWfXXBpn3NlGBkn9NxrqSHu-rYG/view?pli=1
 $PYTHON train_gss_plus.py  --batch_size=2  --datapath $datapath --savepath $savepath --num_epochs 300 --lr 2e-4 --region_fusion_start_epoch 20 --dataname $dataname --pretrain $pretrain
 
-savepath=output_2020_GSS_1
-pretrain="./output_2020_GSS_0/model_last.pth"
+savepath=output_2020_GSS_plus_1
+pretrain="./output_2020_GSS_plus_0/model_last.pth"
 $PYTHON train_gss_plus.py  --batch_size=2  --datapath $datapath --savepath $savepath --num_epochs 300 --lr 2e-4 --region_fusion_start_epoch 20 --dataname $dataname --pretrain $pretrain
 
-savepath=output_2020_GSS_2
-pretrain="./output_2020_GSS_1/model_last.pth" 
+savepath=output_2020_GSS_plus_2
+pretrain="./output_2020_GSS_plus_1/model_last.pth" 
 $PYTHON train_gss_plus.py  --batch_size=2  --datapath $datapath --savepath $savepath --num_epochs 300 --lr 2e-4 --region_fusion_start_epoch 20 --dataname $dataname --pretrain $pretrain
 
-savepath=output_2020_GSS_3
-pretrain="./output_2020_GSS_3/model_last.pth" 
+savepath=output_2020_GSS_plus_3
+pretrain="./output_2020_GSS_plus_3/model_last.pth" 
 $PYTHON train_gss_plus.py  --batch_size=2  --datapath $datapath --savepath $savepath --num_epochs 300 --lr 2e-4 --region_fusion_start_epoch 20 --dataname $dataname --pretrain $pretrain
 
-savepath=output_2020_GSS_4
-pretrain="./output_2020_GSS_4/model_last.pth" 
+savepath=output_2020_GSS_plus_4
+pretrain="./output_2020_GSS_plus_4/model_last.pth" 
 $PYTHON train_gss_plus.py  --batch_size=2  --datapath $datapath --savepath $savepath --num_epochs 300 --lr 2e-4 --region_fusion_start_epoch 20 --dataname $dataname --pretrain $pretrain
